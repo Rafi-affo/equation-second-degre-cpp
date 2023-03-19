299fe1f3787338857beeb9f08c9f778d40d2bfb4
Rafi-affo <afrafiath@gmail.com>
2023-03-19 15:52:21 +0100

    declaration

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
+    double a,b,c,resultat;
+     //Afficher un message a l'utilisateur
+    cout << "Entrer la valeur de a : " << endl;
+    //Lire la valeur saisi par l'utilisateur et l'enregistrer dans la variable a
+    cin >> a;
+    while(a == 0){
+        cout << "Erreur a ne doit pas etre nul. Reessayez" << endl;
+        cin >> a;
+    }
+
+
+    //Demander à l'utilisateur d'entrer b
+    //Afficher un message a l'utilisateur
+    cout << "Entrer la valeur de b : " << endl;
+    //Lire la valeur saisi par l'utilisateur et l'enregistrer dans la variable b
+    cin >> b;
     cout << "hello";
     return 0;
 }
